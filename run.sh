be3ece9905c4add56f46b49726b3904d5a003ca9
Dantalion <hxiangyu@ucsc.edu>
2025-04-28 11:42:40 -0700

    update

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 make
-./prefetch_latency_test
-./nvlink_contention_test
-./host_migration_parallelism_test
+./prefetch_latency_test > prefetch_latency_test.log 2>&1
+./nvlink_contention_test > prefetch_latency_test.log 2>&1
+./host_migration_parallelism_test > prefetch_latency_test.log 2>&1
