0fb91a46ca90a656b907da950e5833dbf478b909
dantalion <dantalion@dantalions-MacBook-Air.local>
2025-05-20 15:53:44 -0700

    debug

--- bleed/runb.sh
+++ bleed/runb.sh
@@ -49,16 +49,16 @@ python3 plot.py ./3logs  ./3plots
 mkdir 4logs
 # Run both programs concurrently
 for size in "${SIZES[@]}"; do
-./prog_b --size $size --local 1 --remote 0> ./4logs/0a1${size}.log  &
+./prog_b --size $size --local 1 --remote 0 > ./4logs/0a1${size}.log  &
 PID_A=$!
 
-./prog_b --size $size --local 0 --remote 1> ./4logs/1a0${size}.log  &
+./prog_b --size $size --local 0 --remote 1 > ./4logs/1a0${size}.log  &
 PID_B=$!
 
-./prog_b --size $size --local 0 --remote 2> ./4logs/2a0${size}.log  &
+./prog_b --size $size --local 0 --remote 2 > ./4logs/2a0${size}.log  &
 PID_B1=$!
 
-./prog_b --size $size --local 0 --remote 3> ./4logs/3a0${size}.log  &
+./prog_b --size $size --local 0 --remote 3 > ./4logs/3a0${size}.log  &
 PID_B2=$!
 
 
@@ -75,19 +75,19 @@ python3 plot.py ./4logs  ./4plots
 mkdir 5logs
 # Run both programs concurrently
 for size in "${SIZES[@]}"; do
-./prog_b --size $size --local 1 --remote 0> ./5logs/0a1${size}.log  &
+./prog_b --size $size --local 1 --remote 0 > ./5logs/0a1${size}.log  &
 PID_A=$!
 
-./prog_b --size $size --local 0 --remote 1> ./5logs/1a0${size}.log  &
+./prog_b --size $size --local 0 --remote 1 > ./5logs/1a0${size}.log  &
 PID_B=$!
 
-./prog_b --size $size --local 0 --remote 2> ./5logs/2a0${size}.log  &
+./prog_b --size $size --local 0 --remote 2 > ./5logs/2a0${size}.log  &
 PID_B1=$!
 
-./prog_b --size $size --local 0 --remote 3> ./5logs/3a0${size}.log  &
+./prog_b --size $size --local 0 --remote 3 > ./5logs/3a0${size}.log  &
 PID_B2=$!
 
-./prog_b --size $size --local 0 --remote 4> ./5logs/4a0${size}.log  &
+./prog_b --size $size --local 0 --remote 4 > ./5logs/4a0${size}.log  &
 PID_B3=$!
 
 # Wait for both to finish
@@ -104,22 +104,22 @@ python3 plot.py ./5logs  ./5plots
 mkdir 6logs
 # Run both programs concurrently
 for size in "${SIZES[@]}"; do
-./prog_b --size $size --local 1 --remote 0> ./6logs/0a1${size}.log  &
+./prog_b --size $size --local 1 --remote 0 > ./6logs/0a1${size}.log  &
 PID_A=$!
 
-./prog_b --size $size --local 0 --remote 1> ./6logs/1a0${size}.log  &
+./prog_b --size $size --local 0 --remote 1 > ./6logs/1a0${size}.log  &
 PID_B=$!
 
-./prog_b --size $size --local 0 --remote 2> ./6logs/2a0${size}.log  &
+./prog_b --size $size --local 0 --remote 2 > ./6logs/2a0${size}.log  &
 PID_B1=$!
 
-./prog_b --size $size --local 0 --remote 3> ./6logs/3a0${size}.log  &
+./prog_b --size $size --local 0 --remote 3 > ./6logs/3a0${size}.log  &
 PID_B2=$!
 
-./prog_b --size $size --local 0 --remote 4> ./6logs/4a0${size}.log  &
+./prog_b --size $size --local 0 --remote 4 > ./6logs/4a0${size}.log  &
 PID_B3=$!
 
-./prog_b --size $size --local 0 --remote 5> ./6logs/5a0${size}.log  &
+./prog_b --size $size --local 0 --remote 5 > ./6logs/5a0${size}.log  &
 PID_B4=$!
 
 # Wait for both to finish
@@ -137,25 +137,25 @@ python3 plot.py ./6logs  ./6plots
 mkdir 7logs
 # Run both programs concurrently
 for size in "${SIZES[@]}"; do
-./prog_b --size $size --local 1 --remote 0> ./7logs/0a1${size}.log  &
+./prog_b --size $size --local 1 --remote 0 > ./7logs/0a1${size}.log  &
 PID_A=$!
 
-./prog_b --size $size --local 0 --remote 1> ./7logs/1a0${size}.log  &
+./prog_b --size $size --local 0 --remote 1 > ./7logs/1a0${size}.log  &
 PID_B=$!
 
-./prog_b --size $size --local 0 --remote 2> ./7logs/2a0${size}.log  &
+./prog_b --size $size --local 0 --remote 2 > ./7logs/2a0${size}.log  &
 PID_B1=$!
 
-./prog_b --size $size --local 0 --remote 3> ./7logs/3a0${size}.log  &
+./prog_b --size $size --local 0 --remote 3 > ./7logs/3a0${size}.log  &
 PID_B2=$!
 
-./prog_b --size $size --local 0 --remote 4> ./7logs/4a0${size}.log  &
+./prog_b --size $size --local 0 --remote 4 > ./7logs/4a0${size}.log  &
 PID_B3=$!
 
-./prog_b --size $size --local 0 --remote 5> ./7logs/5a0${size}.log  &
+./prog_b --size $size --local 0 --remote 5 > ./7logs/5a0${size}.log  &
 PID_B4=$!
 
-./prog_b --size $size --local 0 --remote 6> ./7logs/6a0${size}.log  &
+./prog_b --size $size --local 0 --remote 6 > ./7logs/6a0${size}.log  &
 PID_B5=$!
 
 # Wait for both to finish
@@ -174,28 +174,28 @@ python3 plot.py ./7logs  ./7plots
 mkdir 8logs
 # Run both programs concurrently
 for size in "${SIZES[@]}"; do
-./prog_b --size $size --local 1 --remote 0> ./8logs/0a1${size}.log  &
+./prog_b --size $size --local 1 --remote 0 > ./8logs/0a1${size}.log  &
 PID_A=$!
 
-./prog_b --size $size --local 0 --remote 1> ./8logs/1a0${size}.log  &
+./prog_b --size $size --local 0 --remote 1 > ./8logs/1a0${size}.log  &
 PID_B=$!
 
-./prog_b --size $size --local 0 --remote 2> ./8logs/2a0${size}.log  &
+./prog_b --size $size --local 0 --remote 2 > ./8logs/2a0${size}.log  &
 PID_B1=$!
 
-./prog_b --size $size --local 0 --remote 3> ./8logs/3a0${size}.log  &
+./prog_b --size $size --local 0 --remote 3 > ./8logs/3a0${size}.log  &
 PID_B2=$!
 
-./prog_b --size $size --local 0 --remote 4> ./8logs/4a0${size}.log  &
+./prog_b --size $size --local 0 --remote 4 > ./8logs/4a0${size}.log  &
 PID_B3=$!
 
-./prog_b --size $size --local 0 --remote 5> ./8logs/5a0${size}.log  &
+./prog_b --size $size --local 0 --remote 5 > ./8logs/5a0${size}.log  &
 PID_B4=$!
 
-./prog_b --size $size --local 0 --remote 6> ./8logs/6a0${size}.log  &
+./prog_b --size $size --local 0 --remote 6 > ./8logs/6a0${size}.log  &
 PID_B5=$!
 
-./prog_b --size $size --local 0 --remote 7> ./8logs/7a0${size}.log  &
+./prog_b --size $size --local 0 --remote 7 > ./8logs/7a0${size}.log  &
 PID_B6=$!
 
 # Wait for both to finish
@@ -215,21 +215,21 @@ python3 plot.py ./8logs  ./8plots
 mkdir blogs
 # Run both programs concurrently
 for size in "${SIZES[@]}"; do
-./prog_b --size $size --local 1 --remote 0> ./blogs/0a1${size}.log  
+./prog_b --size $size --local 1 --remote 0 > ./blogs/0a1${size}.log  
 
-./prog_b --size $size --local 0 --remote 1> ./blogs/1a0${size}.log  
+./prog_b --size $size --local 0 --remote 1 > ./blogs/1a0${size}.log  
 
-./prog_b --size $size --local 0 --remote 2> ./blogs/2a0${size}.log  
+./prog_b --size $size --local 0 --remote 2 > ./blogs/2a0${size}.log  
 
-./prog_b --size $size --local 0 --remote 3> ./blogs/3a0${size}.log  
+./prog_b --size $size --local 0 --remote 3 > ./blogs/3a0${size}.log  
 
-./prog_b --size $size --local 0 --remote 4> ./blogs/4a0${size}.log  
+./prog_b --size $size --local 0 --remote 4 > ./blogs/4a0${size}.log  
 
-./prog_b --size $size --local 0 --remote 5> ./blogs/5a0${size}.log  
+./prog_b --size $size --local 0 --remote 5 > ./blogs/5a0${size}.log  
 
-./prog_b --size $size --local 0 --remote 6> ./blogs/6a0${size}.log  
+./prog_b --size $size --local 0 --remote 6 > ./blogs/6a0${size}.log  
 
-./prog_b --size $size --local 0 --remote 7> ./blogs/7a0${size}.log  
+./prog_b --size $size --local 0 --remote 7 > ./blogs/7a0${size}.log  
 
 echo "Completed size=$size"
 done
